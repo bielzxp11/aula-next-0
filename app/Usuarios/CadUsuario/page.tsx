ecf21b7eec2b8c37b80bbf2a437f0aaa4cd2cd3f
bielzxp11 <134931346+bielzxp11@users.noreply.github.com>
2024-03-11 11:46:30 -0300

    aula links 2

--- app/Usuarios/CadUsuario/page.tsx
+++ app/Usuarios/CadUsuario/page.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
+
 export default function CadUsuario () {
     return(
         <>
            <h1>Cadastro</h1> 
            <p>Insira as Informações do usuário</p>
+
+
+     <ul>
+       <li><Link href={'/'}>Voltar</Link></li>
+     </ul>  
+
         </>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+import Link from 'next/link'
+
 export default function Page() {
   return(
     <>
         <h1>Hello, Next.js!</h1>
         <h2>Título 2</h2>
         <h3>Título 3</h3>
-        qualquer coisa
-
-       <Link href={'/sobre'}>Sobre</Link>
-       <Link href={'/CadUsuario'}>Cadastro de Usuario</Link> 
+        Qualquer coisa
+        
+     <ul>
+     <br/>
+       <li><Link href={'/sobre'}>Sobre</Link></li><br/>
+       <li><Link href={'/CadUsuario'}>Cadastro de Usuario</Link></li>
+     </ul>  
     </>
-  )
+  );
 }
