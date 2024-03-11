9e59886028d569ee50b0aeab84e5b5d2aaabce0f
bielzxp11 <134931346+bielzxp11@users.noreply.github.com>
2024-03-11 09:49:21 -0300

    aula link

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return(
+    <>
+        <h1>Hello, Next.js!</h1>
+        <h2>Título 2</h2>
+        <h3>Título 3</h3>
+        qualquer coisa
+
+       <Link href={'/sobre'}>Sobre</Link>
+       <Link href={'/CadUsuario'}>Cadastro de Usuario</Link> 
+    </>
+  )
 }
